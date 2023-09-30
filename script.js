7892b5ff4075500e9631c1f6efd61f35428dcfd4
Somu <74103728+Somu878@users.noreply.github.com>
2023-09-30 16:10:07 +0530

    max of 3 dec result #5

--- script.js
+++ script.js
@@ -5,21 +5,24 @@ const buttons = document.querySelectorAll(".element");
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const btext = button.innerText;
+        //removes the current button text
         if (btext==='DEL'){
             output.innerText =output.innerText.slice(0,-1);
         }
+        //resets the output.innertext
         else if(btext ==='RESET'){
             output.innerText=" "
         }
+        //displays final evaluated result
         else if(btext==='='){
            let res= output.innerText.replace(/x/g, '*');
            output.innerText=eval(res)
+           //limiting maximum digits to 3 afterr decimal point
+           output.innerText = parseFloat(res).toFixed(3);
         }
         else{
             output.innerText += btext;
 
         }
-
-        
     }); 
 });
