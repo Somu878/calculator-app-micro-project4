fbbc6da0e4cefb424476efa3dee1bbff7461c90e
Somu <74103728+Somu878@users.noreply.github.com>
2023-09-30 15:36:20 +0530

    added js file #2

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// Get elements
+const output = document.getElementById("output");
+const buttons = document.querySelectorAll(".element");
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const btext = button.innerText;
+        if (btext==='DEL'){
+            output.innerText =output.innerText.slice(0,-1);
+        }
+        else if(btext ==='RESET'){
+            output.innerText=" "
+        }
+        else if(btext==='='){
+           let res= output.innerText.replace(/x/g, '*');
+           output.innerText=eval(res)
+        }
+        else{
+            output.innerText += btext;
+
+        }
+
+        
+    }); 
+});
