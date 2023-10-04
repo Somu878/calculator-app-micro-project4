c0eab8ed12f40bcdcb7e5a65058229cc5187af0b
Somu <74103728+Somu878@users.noreply.github.com>
2023-10-04 18:57:37 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-// Get elements
+// Get DOM elements
 const output = document.getElementById("output");
 const buttons = document.querySelectorAll(".element");
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const btext = button.innerText;
-        //removes the current button text
-        if (btext==='DEL'){
-            output.innerText =output.innerText.slice(0,-1);
-        }
-        //resets the output.innertext
-        else if(btext ==='RESET'){
-            output.innerText=" "
-        }
-        //displays final evaluated result
-        else if(btext==='='){
-           let res= output.innerText.replace(/x/g, '*');
-           output.innerText=eval(res)
-        }
-        else{
-            output.innerText += btext;
-
-        }
-    }); 
+let resDisplayed = false;
+output.innerText = "";
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const btext = button.innerText;
+    //removes the current button text
+    if (btext === "DEL") {
+      if (!resDisplayed) {
+        deleteDisplay();
+      }
+    }
+    //resets the output
+    else if (btext === "RESET") {
+      resetDisplay();
+      resDisplayed = false;
+    }
+    //displays final evaluated result
+    else if (btext === "=") {
+      calculate();
+    } else {
+      output.innerText += btext;
+    }
+  });
 });
+//function to delete last input value
+function deleteDisplay() {
+  output.innerText = output.innerText.slice(0, -1);
+}
+
+//function to reset display
+function resetDisplay() {
+  output.innerText = "0";
+}
+
+//function to calculate result
+function calculate() {
+  let res = output.innerText.replace(/x/g, "*");
+  let result = eval(res);
+  // Check if the result is a floating-point number
+  if (Number.isFinite(result) && result % 1 !== 0) {
+    result = result.toFixed(3);
+  }
+  output.innerText = result;
+  resDisplayed = true;
+}
